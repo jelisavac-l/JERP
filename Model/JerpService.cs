1cce20d2553add3a10a70862ba3899533c24b683
jelisavac-l <jelisavacluka63@gmail.com>
2026-03-23 01:20:42 +0100

    feat: added LB policies

--- Model/JerpService.cs
+++ Model/JerpService.cs
@@ -4,14 +4,23 @@ namespace NNTReverseProxy.Model;
 
 public class JerpService
 {
+
+    /*
+     * rr - Round-Robin: Goes round like a record baby
+     * wrr - Weighted Round-Robin: Assigns weight to the rr algorithm
+     * iph - IP (Consistent) Hashing: Always routes same client to the same instance 
+     */
+    public static readonly List<string> LoadBalancingPolicies = ["rr", "wrr", "iph", "rnd"];
+    
     public required string Name { get; set; }
     public required string Path { get; set; }
     public required List<JerpInstance> Instances { get; set; }
-
+    
     public string? LoadBalancingPolicy { get; set; }
     public bool StripPrefix { get; set; } = false;
     public int? Timeout { get; set; }
     public int Retries { get; set; } = 0;
+    
     public Dictionary<string, string>? AddHeaders { get; set; }
     
 }
